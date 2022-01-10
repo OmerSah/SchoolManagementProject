f68b03297defbb8a390cbf5ff9cddc1dfb4db4fc
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2022-01-11 00:10:32 +0300

    Update DBConnection.java

--- SchoolManagementSystem/src/DBConnection.java
+++ SchoolManagementSystem/src/DBConnection.java
@@ -13,11 +13,13 @@ public class DBConnection {
     private Connection conn;
     private static DBConnection dbConnection;
     private String password;
+    private String url;
+    private String user;
     
     private DBConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys", "root", this.password);
+            this.conn = DriverManager.getConnection(this.url, this.user, this.password);
         } catch (ClassNotFoundException | SQLException ex) {
             System.out.println("Database Connection Creation Failed : " + ex.getMessage());
         }
